sourcegraph/scip-ruby

Add FoundDefinitionHashes to FileHash

core/FileHash.h
@@ -110,6 +110,8 @@ struct FoundDefinitionHash {
 };
 CheckSize(FoundDefinitionHash, 12, 4);
 
+using FoundDefinitionHashes = std::vector<FoundDefinitionHash>;
+
 // When a file is edited, we run index and resolve it using an local (empty) GlobalState.
 // We then hash the symbols defined in that local GlobalState, and use the result to quickly decide
 // whether "something" changed, or whether nothing changed (and thus we can take the fast path).
@@ -215,9 +217,11 @@ struct UsageHash {
 struct FileHash {
     LocalSymbolTableHashes localSymbolTableHashes;
     UsageHash usages;
+    FoundDefinitionHashes foundDefinitionHashes;
 
     FileHash() = default;
-    FileHash(LocalSymbolTableHashes &&localSymbolTableHashes, UsageHash &&usages);
+    FileHash(LocalSymbolTableHashes &&localSymbolTableHashes, UsageHash &&usages,
+             FoundDefinitionHashes &&foundDefinitionHashes);
 };
 
 }; // namespace sorbet::core

hashing/hashing.cc
@@ -69,7 +69,8 @@ unique_ptr<core::FileHash> computeFileHashForAST(spdlog::logger &logger, unique_
             auto view = string_view{result.GetString(), result.GetLength()};
             logger.debug(view);
 
-            return make_unique<core::FileHash>(core::LocalSymbolTableHashes::invalidParse(), move(usageHash));
+            return make_unique<core::FileHash>(core::LocalSymbolTableHashes::invalidParse(), move(usageHash),
+                                               core::FoundDefinitionHashes{});
         }
     }
 
@@ -78,9 +79,10 @@ unique_ptr<core::FileHash> computeFileHashForAST(spdlog::logger &logger, unique_
 
     core::Context ctx(*lgs, core::Symbols::root(), single[0].file);
     auto workers = WorkerPool::create(0, lgs->tracer());
-    realmain::pipeline::resolve(lgs, move(single), opts(), *workers);
+    core::FoundDefinitionHashes foundDefinitionHashes; // out parameter
+    realmain::pipeline::resolve(lgs, move(single), opts(), *workers, &foundDefinitionHashes);
 
-    return make_unique<core::FileHash>(move(*lgs->hash()), move(usageHash));
+    return make_unique<core::FileHash>(move(*lgs->hash()), move(usageHash), move(foundDefinitionHashes));
 }
 
 // Note: lgs is an outparameter.

infer/test/infer_test.cc
@@ -41,7 +41,8 @@ void processSource(core::GlobalState &cb, string str) {
     vector<ast::ParsedFile> trees;
     trees.emplace_back(move(tree));
     auto workers = WorkerPool::create(0, *logger);
-    trees = move(namer::Namer::run(cb, move(trees), *workers).result());
+    core::FoundDefinitionHashes foundDefinitionHashes; // compute this just for test coverage
+    trees = move(namer::Namer::run(cb, move(trees), *workers, &foundDefinitionHashes).result());
     auto resolved = resolver::Resolver::run(cb, move(trees), *workers);
     for (auto &tree : resolved.result()) {
         sorbet::core::MutableContext ctx(cb, core::Symbols::root(), tree.file);

namer/namer.h
@@ -1,6 +1,7 @@
 #ifndef SORBET_NAMER_NAMER_H
 #define SORBET_NAMER_NAMER_H
 #include "ast/ast.h"
+#include "core/FileHash.h"
 #include <memory>
 
 namespace sorbet {
@@ -13,8 +14,14 @@ class Namer final {
 public:
     static ast::ParsedFilesOrCancelled
     symbolizeTreesBestEffort(const core::GlobalState &gs, std::vector<ast::ParsedFile> trees, WorkerPool &workers);
+
+    // Note: foundDefinitionHashes is an optional out parameter.
+    //
+    // Setting it to a non-nullptr requests that Namer compute a fingerprint of the FoundDefinitions
+    // it found while running. (Thus, it's usually nullptr except when pipeline::resolve is called
+    // for the purpose of computing a FileHash.)
     static ast::ParsedFilesOrCancelled run(core::GlobalState &gs, std::vector<ast::ParsedFile> trees,
-                                           WorkerPool &workers);
+                                           WorkerPool &workers, core::FoundDefinitionHashes *foundDefinitionHashesOut);
 
     Namer() = delete;
 };

namer/test/namer_test.cc
@@ -47,7 +47,8 @@ vector<ast::ParsedFile> runNamer(core::GlobalState &gs, ast::ParsedFile tree) {
     vector<ast::ParsedFile> v;
     v.emplace_back(move(tree));
     auto workers = WorkerPool::create(0, *logger);
-    return move(namer::Namer::run(gs, move(v), *workers).result());
+    core::FoundDefinitionHashes foundDefinitionHashes; // compute this just for test coverage
+    return move(namer::Namer::run(gs, move(v), *workers, &foundDefinitionHashes).result());
 }
 
 } // namespace

pjambet/sorbet

core/FileHash.h
@@ -118,6 +118,8 @@ struct FoundMethodHash {
 };
 CheckSize(FoundMethodHash, 16, 4);
 
+using FoundMethodHashes = std::vector<FoundMethodHash>;
+
 // When a file is edited, we run index and resolve it using an local (empty) GlobalState.
 // We then hash the symbols defined in that local GlobalState, and use the result to quickly decide
 // whether "something" changed, or whether nothing changed (and thus we can take the fast path).
@@ -239,9 +241,11 @@ struct UsageHash {
 struct FileHash {
     LocalSymbolTableHashes localSymbolTableHashes;
     UsageHash usages;
+    FoundMethodHashes foundMethodHashes;
 
     FileHash() = default;
-    FileHash(LocalSymbolTableHashes &&localSymbolTableHashes, UsageHash &&usages);
+    FileHash(LocalSymbolTableHashes &&localSymbolTableHashes, UsageHash &&usages,
+             FoundMethodHashes &&foundMethodHashes);
 };
 
 }; // namespace sorbet::core

hashing/hashing.cc
@@ -69,7 +69,8 @@ unique_ptr<core::FileHash> computeFileHashForAST(spdlog::logger &logger, unique_
             auto view = string_view{result.GetString(), result.GetLength()};
             logger.debug(view);
 
-            return make_unique<core::FileHash>(core::LocalSymbolTableHashes::invalidParse(), move(usageHash));
+            return make_unique<core::FileHash>(core::LocalSymbolTableHashes::invalidParse(), move(usageHash),
+                                               core::FoundMethodHashes{});
         }
     }
 
@@ -77,9 +78,10 @@ unique_ptr<core::FileHash> computeFileHashForAST(spdlog::logger &logger, unique_
     single.emplace_back(move(file));
 
     auto workers = WorkerPool::create(0, lgs->tracer());
-    realmain::pipeline::resolve(lgs, move(single), opts(), *workers);
+    core::FoundMethodHashes foundMethodHashes; // out parameter
+    realmain::pipeline::resolve(lgs, move(single), opts(), *workers, &foundMethodHashes);
 
-    return make_unique<core::FileHash>(move(*lgs->hash()), move(usageHash));
+    return make_unique<core::FileHash>(move(*lgs->hash()), move(usageHash), move(foundMethodHashes));
 }
 
 // Note: lgs is an outparameter.

infer/test/infer_test.cc
@@ -41,7 +41,8 @@ void processSource(core::GlobalState &cb, string str) {
     vector<ast::ParsedFile> trees;
     trees.emplace_back(move(tree));
     auto workers = WorkerPool::create(0, *logger);
-    trees = move(namer::Namer::run(cb, move(trees), *workers).result());
+    core::FoundMethodHashes foundMethodHashes; // compute this just for test coverage
+    trees = move(namer::Namer::run(cb, move(trees), *workers, &foundMethodHashes).result());
     auto resolved = resolver::Resolver::run(cb, move(trees), *workers);
     for (auto &tree : resolved.result()) {
         sorbet::core::MutableContext ctx(cb, core::Symbols::root(), tree.file);

namer/namer.h
@@ -1,6 +1,7 @@
 #ifndef SORBET_NAMER_NAMER_H
 #define SORBET_NAMER_NAMER_H
 #include "ast/ast.h"
+#include "core/FileHash.h"
 #include <memory>
 
 namespace sorbet {
@@ -13,8 +14,14 @@ class Namer final {
 public:
     static ast::ParsedFilesOrCancelled
     symbolizeTreesBestEffort(const core::GlobalState &gs, std::vector<ast::ParsedFile> trees, WorkerPool &workers);
+
+    // Note: foundMethodHashes is an optional out parameter.
+    //
+    // Setting it to a non-nullptr requests that Namer compute a fingerprint of the FoundDefinitions
+    // it found while running. (Thus, it's usually nullptr except when pipeline::resolve is called
+    // for the purpose of computing a FileHash.)
     static ast::ParsedFilesOrCancelled run(core::GlobalState &gs, std::vector<ast::ParsedFile> trees,
-                                           WorkerPool &workers);
+                                           WorkerPool &workers, core::FoundMethodHashes *foundMethodHashesOut);
 
     Namer() = delete;
 };

namer/test/namer_test.cc
@@ -47,7 +47,8 @@ vector<ast::ParsedFile> runNamer(core::GlobalState &gs, ast::ParsedFile tree) {
     vector<ast::ParsedFile> v;
     v.emplace_back(move(tree));
     auto workers = WorkerPool::create(0, *logger);
-    return move(namer::Namer::run(gs, move(v), *workers).result());
+    core::FoundMethodHashes foundMethodHashes; // compute this just for test coverage
+    return move(namer::Namer::run(gs, move(v), *workers, &foundMethodHashes).result());
 }
 
 } // namespace